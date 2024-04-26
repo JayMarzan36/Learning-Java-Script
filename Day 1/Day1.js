287fa4c2264ef2830ff152b306ccf50ba0efbaa4
Jay Marzan <jaymblue5@gmail.com>
2024-04-25 21:09:45 -0600

    Starting small

---
+++ Day 1/Day1.js
@@ -0,0 +1,25 @@
+// I'm going to try to figure out what I can do without any tutorials
+// Starting out small
+console.log("Hello World")
+
+let a = 5
+let b = 5
+console.log(a + b)
+
+console.log(a + '5')
+
+console.error("Error message?")
+
+b = 6
+console.assert(a+b == 10) // Built in unit tests!?
+
+
+let c = ('1', '2', '3')
+console.count(c)
+
+// I have a python background so I had to try this out
+for (i in c.length) {
+    console.log(i)
+}
+
+// console.clear(), commented for obvious reasons after I found out what it did
